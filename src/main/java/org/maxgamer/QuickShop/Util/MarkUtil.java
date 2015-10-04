500e94cb2980aecc8ba35b18f4c84936a97da6ed
j502647092 <jtb1@163.com>
2015-10-05 02:10:19 +0800

    add lose file...
    Signed-off-by: j502647092 <jtb1@163.com>

---
+++ src/main/java/org/maxgamer/QuickShop/Util/MarkUtil.java
@@ -0,0 +1,23 @@
+package org.maxgamer.QuickShop.Util;
+
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class MarkUtil {
+	static String mark = "§q§s§r";
+
+	public static void addMark(final ItemStack ci) {
+		final ItemMeta meta = ci.getItemMeta();
+		meta.setDisplayName(ChatColor.RED + "QuickShop " + Util.getName(ci));
+		ci.setItemMeta(meta);
+	}
+
+	public static boolean hasMark(final ItemStack ci) {
+		try {
+			return ci.getItemMeta().getDisplayName().startsWith(mark);
+		} catch (final Exception e) {
+			return false;
+		}
+	}
+}
