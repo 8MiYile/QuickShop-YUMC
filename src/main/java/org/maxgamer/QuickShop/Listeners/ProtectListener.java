4ab5a1aa671e52b6485a1511b6a529a91ffe20ea
502647092 <jtb1@163.com>
2016-02-19 09:34:48 +0800

    修复一个NPE错误...
    Signed-off-by: 502647092 <jtb1@163.com>

--- src/main/java/org/maxgamer/QuickShop/Listeners/ProtectListener.java
+++ src/main/java/org/maxgamer/QuickShop/Listeners/ProtectListener.java
@@ -61,7 +61,8 @@ public class ProtectListener implements Listener {
         final Shop srcshop = getShop(src);
         final Shop meshop = getShop(me);
         final Shop desshop = getShop(des);
-        if ((srcshop != null && meshop == null) || (meshop != null && desshop == null) || (srcshop != null && desshop != null && srcshop.getOwner().equalsIgnoreCase(desshop.getOwner()))) {
+        if ((srcshop != null && meshop == null) || (meshop != null && desshop == null)
+                || (srcshop != null && desshop != null && srcshop.getOwner() != null && srcshop.getOwner().equalsIgnoreCase(desshop.getOwner()))) {
             e.setCancelled(true);
         }
     }
